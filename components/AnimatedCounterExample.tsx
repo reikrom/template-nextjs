79a41156d5c5db33a84de530d764bbc995d7b3b6
Rei Krom <mr.mygo@gmail.com>
2021-10-01 22:06:48 +0100

    set up linting

--- components/AnimatedCounterExample.tsx
+++ components/AnimatedCounterExample.tsx
@@ -5,29 +5,27 @@ interface IProps {
     counter: number;
 }
 
-const AnimatedCounterExample = ({ counter }: IProps) => {
-    return (
-        <AnimatePresence>
-            <m.div
-                key={counter}
-                // initial={{ y: -150, opacity: 0 }}
-                animate={{ y: 0, opacity: 1 }}
-                exit={{ y: -200, opacity: 0, position: 'absolute' }}
-                transition={{
-                    ease: 'easeOut',
-                    damping: 10,
-                    stiffness: 100,
-                    duration: 1,
-                }}
-                whileHover={{ scale: 1.2 }}
-                whileTap={{ scale: 0.8 }}
-                // style={{ x: 100 }}
-                className=" text-9xl cursor-pointer select-none"
-            >
-                {counter}
-            </m.div>
-        </AnimatePresence>
-    );
-};
+const AnimatedCounterExample = ({ counter }: IProps) => (
+    <AnimatePresence>
+        <m.div
+            key={counter}
+            // initial={{ y: -150, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: -200, opacity: 0, position: 'absolute' }}
+            transition={{
+                ease: 'easeOut',
+                damping: 10,
+                stiffness: 100,
+                duration: 1,
+            }}
+            whileHover={{ scale: 1.2 }}
+            whileTap={{ scale: 0.8 }}
+            // style={{ x: 100 }}
+            className=" text-9xl cursor-pointer select-none"
+        >
+            {counter}
+        </m.div>
+    </AnimatePresence>
+);
 
 export default AnimatedCounterExample;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-// import '../styles/globals.css';
+import React from 'react';
 import 'tailwindcss/tailwind.css';
 import type { AppProps } from 'next/app';
 
